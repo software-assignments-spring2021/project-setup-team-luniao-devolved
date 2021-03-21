76432d034aea85d3bd30f30d1ac2b0873ae210b1
Kaylee Park <yp1068@nyu.edu>
2021-03-21 01:00:24 -0400

    created guest dashboard

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './index.css';
 import Login from './Login';
+import GuestDashboard from './GuestDashboard';
 
 ReactDOM.render(
   <Router>
        <Switch>
 		      <Route exact path="/" component={Login}/>
+          <Route exact path="/guestdashboard" component={GuestDashboard}/>
 	    </Switch>
   </Router>,
   document.getElementById('root')
