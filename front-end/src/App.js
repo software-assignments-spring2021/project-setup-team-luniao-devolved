577687c9f0be1b76ee3a260a438ce42ea00b7da1
kartik57 <kj1140@nyu.edu>
2021-03-22 03:58:07 +0530

    Created Preferences

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
+
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    return (
+        // Container with padding
+        <Container className="p-3">
+                // hiiii
+            <Jumbotron>
+                <h1>Trip Preferences</h1>
+                <p>
+                    Customize preferences for your upcoming trip!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            </Jumbotron>
+            <Form>
+                <Form.Group controlId="TripBudget">
+                    <Form.Label>Trip Budget</Form.Label>
+                    <Form.Control size="sm" type="text" placeholder="$" />
+                </Form.Group>
+                <Form.Group controlId="DepartureTime">
+                    <Form.Label>Departure Time</Form.Label>
+                    <Form.Control size="sm" as="select" >
+                        <option>Morning</option>
+                        <option>Afternoon</option>
+                        <option>Evening</option>
+                        <option>Night</option>
+                        <option>Red-Eye</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group controlId="Duration">
+                    <Form.Label>Duration</Form.Label>
+                    <Form.Control size="sm" type="text" placeholder="Number of Nights" />
+                </Form.Group>
+                <Form.Group controlId="StayType">
+                    <Form.Label>Stay Type</Form.Label>
+                    <Form.Control size="sm" as="select">
+                        <option>Hotel</option>
+                        <option>Apartment</option>
+                        <option>Home Stay</option>
+                        <option>Hostel</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group controlId="Rating">
+                    <Form.Label>Rating</Form.Label>
+                    <Form.Control size="sm" as="select">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3 </option>
+                        <option>4</option>
+                        <option>5</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group controlId="Transportation">
+                    <Form.Label>Transportation</Form.Label>
+                    <Form.Control size="sm" as="select">
+                        <option>Flight</option>
+                        <option>Train</option>
+                        <option>Bus</option>
+                        <option>Personal</option>
+                    </Form.Control>
+                </Form.Group>
+            </Form >
+            <>
+
+                <Button variant="outline-danger">Cancel</Button>{' '}
+                <Button variant="outline-success">Confirm</Button>{' '}
+                <br />
+
+            </>
+
+
+            <>
+                <br />
+                <Button variant="outline-secondary" href="./Cpoll" size="lg" block>Page 2- Poll </Button> {' '}
+            </>
+
+        </Container >
+
+
+    );
 }
+
+
+
 
 export default App;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import App from "./App";
+import Cpoll from "./Cpoll";
+import Itinerary from "./Itinerary";
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/Cpoll" component={Cpoll} />
+      <Route path="/Itinerary" component={Itinerary} />
+    </Switch>
+  </BrowserRouter>,
+  rootElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
