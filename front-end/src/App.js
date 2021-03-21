c0f443cb60f5bee81ce8d45e1f63813d94a0ab5b
Pranav <pg1653@nyu.edu>
2021-03-21 20:04:31 +0530

    creating react app and recommendations page (tasks 91, 93)

---
+++ front-end/src/App.js
@@ -0,0 +1,171 @@
+// import logo from 'logo2.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import './App.css';
+import { useState } from 'react';
+
+import { Alert, Form, Button, Card, Modal, Container, Row, Col} from 'react-bootstrap';
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Recommendations
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Container>
+
+          {props.recs.map(e => (
+            <Row className="rec-row">
+              <Card className="rec-card">
+                <Card.Body>
+                  <Card.Title>{e[0]}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">{e[1]} </Card.Subtitle>
+                  <Card.Text>
+                    {e[2]}
+                  </Card.Text>
+                  <Card.Link href={e[3]}>More info</Card.Link>
+                </Card.Body>
+              </Card>
+          </Row>
+          ))}
+
+        </Container>
+      </Modal.Body>
+      
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+
+function App(props) {
+
+  const [topic, setTopic] = useState(0);
+  const [budget, setBudget] = useState(0);
+  const [date, setDate] = useState("");
+  const [show, setShow] = useState(false);
+  const [showFlightVar, setFlight] = useState(true);
+  const [showOtherVar, setOther] = useState(false);
+  
+  const [recsReceived, setRecsReceived] = useState([]);
+  const [modalShow, setModalShow] = useState(false);
+
+
+  const handleSubmit= (e) => {
+    console.log("Form values saved.");
+    setShow(true);
+    e.preventDefault();
+
+    setModalShow(true)
+    // submit values, and return recommendations from back-end
+    // creating test data with example format
+    setRecsReceived([["3/22/2021", "LAX to JFK", "$555", "example.org/1"], ["3/23/2021", "LAX to JFK", "$783", "example.org/2"]]); 
+  }
+
+  let showFunc = null;
+  if (show === true) {
+    showFunc = <Alert variant="success" onClose={() => setShow(false)} dismissible> Form submitted.</Alert>;
+  }
+
+  let showFlightForm = null;
+  if (showFlightVar) {
+    showFlightForm = (
+      <div>
+        <Form.Group controlId="validationCustom05">
+          <Form.Label>Budget (in $)</Form.Label>
+          <Form.Control type="number" placeholder="100" required value={budget} onChange={e => setBudget(e.target.value)}/>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid number.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group controlId="validationCustom05">
+          <Form.Label>Date</Form.Label>
+          <Form.Control type="date" placeholder="100" required value={date} onChange={e => setDate(e.target.value)}/>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid date.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </div>)
+  }
+
+  let showOtherForm = null;
+  if (showOtherVar) {
+    showOtherForm = (
+      <div>
+        <p>Other form</p>
+      </div>
+    )
+  }
+
+  // let showRecs = null;
+  // if (typeof recsReceived !== 'undefined' && recsReceived.length > 0) {
+  //   showRecs = (
+  //     <div>
+       
+  //     </div>
+  //   )
+  // }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo2.png" className="logo" alt="logo" />
+        {<h2 className="App-title">Recommendations</h2>}
+      </header>
+
+      {showFunc} 
+
+      <body className="App-body">
+          <Form className="App-form" onSubmit={e => { handleSubmit(e) }}>
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Label>Topic</Form.Label>
+              <Form.Control as="select" value={topic} onChange={e => {
+                setTopic(e.target.value)
+                if (e.target.value === "0") {
+                  setFlight(true);
+                  setOther(false);
+                } else if (e.target.value === "-1") {
+                  setFlight(false);
+                  setOther(true);
+                }
+                }}>
+                <option value="0">Flight</option>
+                <option value="-1">Other</option>
+              </Form.Control>
+            </Form.Group>
+            
+            {showFlightForm} {showOtherForm}
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+
+         {/* {showRecs} */}
+
+          <MyVerticallyCenteredModal
+            recs={recsReceived}
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
+
+      </body>
+
+
+    </div>
+  );
+}
+
+export default App;
