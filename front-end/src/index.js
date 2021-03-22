b1655dccdf5247ab79ccadf1bc597ffbef7db5f3
kartik57 <kj1140@nyu.edu>
2021-03-23 01:30:51 +0530

    Removed unneeded files for merge

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-// Importing the Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './index.css';
+import Login from './Login';
+import GuestDashboard from './GuestDashboard';
+import SignUp from './SignUp';
+import CreatePost from './CreatePost';
+import Recommendations from './Recommendations';
+import PastTrips from './PastTrips'
 
-import App from "./App";
-import Cpoll from "./Cpoll";
-import Itinerary from "./Itinerary";
-
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/Cpoll" component={Cpoll} />
-      <Route path="/Itinerary" component={Itinerary} />
-    </Switch>
-  </BrowserRouter>,
-  rootElement
+  <Router>
+       <Switch>
+		      <Route exact path="/" component={Login}/>
+          <Route exact path="/guestdashboard" component={GuestDashboard}/>
+          <Route exact path="/signup" component={SignUp}/>
+          <Route exact path="/createpost" component={CreatePost}/>
+          <Route exact path="/recommendations" component={Recommendations}/>
+          <Route exact path="/pasttrips" component={PastTrips}/>
+	    </Switch>
+  </Router>,
+  document.getElementById('root')
 );
-
-
-
