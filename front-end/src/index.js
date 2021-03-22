3ab30e31cc008553de77ef92d4dda711bec784bf
kartik57 <kj1140@nyu.edu>
2021-03-23 01:23:01 +0530

    Fixed errors

---
+++ front-end/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './index.css';
+import Login from './Login';
+import GuestDashboard from './GuestDashboard';
+import SignUp from './SignUp';
+import CreatePost from './CreatePost';
+import Recommendations from './Recommendations';
+import PastTrips from './PastTrips'
+
+ReactDOM.render(
+  <Router>
+       <Switch>
+		      <Route exact path="/" component={Login}/>
+          <Route exact path="/guestdashboard" component={GuestDashboard}/>
+          <Route exact path="/signup" component={SignUp}/>
+          <Route exact path="/createpost" component={CreatePost}/>
+          <Route exact path="/recommendations" component={Recommendations}/>
+          <Route exact path="/pasttrips" component={PastTrips}/>
+	    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
