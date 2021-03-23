e02c6ac0c9e673f429e0e0a1bffa327b5c2427c7
Kaylee Park <yp1068@nyu.edu>
2021-03-22 23:21:23 -0400

    css working for dashboard

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-//import './index.css';
+import './index.css';
 import Login from './Login';
 import GuestDashboard from './GuestDashboard';
 import SignUp from './SignUp';
-//import CreatePost from './CreatePost';
-//import Recommendations from './Recommendations';
-//import PastTrips from './PastTrips';
 import Dashboard from './Dashboard';
 
 ReactDOM.render(
