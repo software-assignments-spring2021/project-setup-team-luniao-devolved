46bd360d60912f3684d574b00b56e79d48d0fc4b
Kaylee Park <yp1068@nyu.edu>
2021-03-22 23:13:13 -0400

    checking

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,9 +5,9 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './Login';
 import GuestDashboard from './GuestDashboard';
 import SignUp from './SignUp';
-import CreatePost from './CreatePost';
-import Recommendations from './Recommendations';
-import PastTrips from './PastTrips';
+//import CreatePost from './CreatePost';
+//import Recommendations from './Recommendations';
+//import PastTrips from './PastTrips';
 import Dashboard from './Dashboard';
 
 ReactDOM.render(
@@ -16,9 +16,6 @@ ReactDOM.render(
 		      <Route exact path="/" component={Login}/>
           <Route exact path="/guestdashboard" component={GuestDashboard}/>
           <Route exact path="/signup" component={SignUp}/>
-          <Route exact path="/createpost" component={CreatePost}/>
-          <Route exact path="/recommendations" component={Recommendations}/>
-          <Route exact path="/pasttrips" component={PastTrips}/>
           <Route exact path="/dashboard" component={Dashboard}/>
 	    </Switch>
   </Router>,
