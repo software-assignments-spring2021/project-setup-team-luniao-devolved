19d0816540e929c0ca7176bda7246f30074d1cfe
Emma Sargent <emma.sargent@publicissapient.com>
2021-04-04 19:08:46 -0400

    EditNewTripCSS

--- front-end/src/CurrentTrip.js
+++ front-end/src/CurrentTrip.js
@@ -1,57 +1,81 @@
 import React, { useState, useEffect } from 'react'
-
-import './CurrentTrip.css'
+import './CurrentTrip.css';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
+
 const CurrentTrip = (props) => {
 
   return (
     <div className="CurrentTrip">
-      <h3>Current Trip</h3>
+
       <section className="main-content">
+        <h1>Current Trip</h1>
         <div class='flex-container'>
           <div>
-            <h4>Trip Title:</h4>
+            <h3>Trip Title</h3>
           </div>
 
-          <div className="friends">
+          <div className="current-trip-friends">
             <p>Friends: </p>
 
-            <ListGroup horizontal={'sm'}>
+            <ul className="friends-list">
+              <li>
+                
+                NAME
+              </li>
+
+              <li>
+                
+                NAME
+              </li>
+
+              <li>
+                
+                NAME
+              </li>
+
+              <li>
+                
+                NAME
+              </li>
+            </ul>
+
+            {/* <ListGroup className="current-trip-friends">
               <ListGroup.Item>
-                <img src="logo2.png" alt=""></img>
+                <img src="logo192.png" alt=""></img>
                 Pranav Guntunur
               </ListGroup.Item>
 
               <ListGroup.Item>
-                <img src="logo2.png" alt=""></img>
+                <img src="logo192.png" alt=""></img>
                 Karik Jain
               </ListGroup.Item>
 
               <ListGroup.Item>
-                <img src="logo2.png" alt=""></img>
+                <img src="logo192.png" alt=""></img>
                 Kaylee Park
               </ListGroup.Item>
 
               <ListGroup.Item>
-                <img src="logo2.png" alt=""></img>
+                <img src="logo192.png" alt=""></img>
                 Brian Steinberg
               </ListGroup.Item>
-            </ListGroup>
+            </ListGroup> */}
           </div>
 
-          <div className="links">
-            <Button href="/itinerary">Full Itinerary</Button>
-            <Button href="/addfriends">Add Friends</Button>
-            <Button href="/createpoll">Polls</Button>
-            <Button href="/recommendations">Recommendations</Button>
+          <div className="current-trip-btns">
+            <Button href="/Itinerary">Full Itinerary</Button>
+            <Button href="#">Add Friends</Button>
+            <Button href="#">Polls</Button>
+            <Button href="/Recommendations">Recommendations</Button>
           </div>
 
-          <div className="todo"> 
+          <div className="current-trip-todo">
             <p>To-do List</p>
 
-            <ListGroup>
+            <ListGroup class="list-group list-group-flush">
               <ListGroup.Item>
+
                 Venmo for Tickets
                 <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
               </ListGroup.Item>
@@ -63,15 +87,16 @@ const CurrentTrip = (props) => {
               </ListGroup.Item>
 
               <ListGroup.Item>
+
                 Research tourist destinations
                 <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
               </ListGroup.Item>
             </ListGroup>
           </div>
-          <br />
-          <div>
-            <Button href="/dashboard">Back</Button>
-          </div>
+
+          {/* <div>
+            <Button href="/">Back</Button>
+          </div> */}
 
         </div>
       </section>
