95d1644957124e3aa957d1c514fef8b296bfeaff
Pranav <pg1653@nyu.edu>
2021-04-04 19:14:26 +0530

    created get route for pastTrips

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,35 @@
 // import and instantiate express
 const express = require("express") // CommonJS import style!
+const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
+const axios = require("axios")
+
 const app = express() // instantiate an Express object
 
+app.use(morgan("dev"))
+
 // we will put some server logic here later...
+
+
+/* Past Trips Page Routes */
+// An api endpoint that returns list of past trips
+app.get('/api/pasttrips', (req,res) => {
+
+    /*
+    Once mongoose is setup, we would retrieve the data of past trips stored by unique user id. However, as we don't
+    mongo set up, I just retrieve mock data from mockaroo.
+    */
+    axios
+    .get("https://my.api.mockaroo.com/past-trips.json?key=8f9d78c0")
+    .then(pastTrips => {
+        
+        res.json(pastTrips.data);
+        console.log(pastTrips.data)
+        console.log('Retrieved past trips');
+        }) // pass data along directly to client
+    .catch(err => next(err)) // pass any errors to express
+});
+
+
 
 // export the express app we created to make it available to other modules
 module.exports = app
