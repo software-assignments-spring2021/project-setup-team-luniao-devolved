1a1a8b4aa550a1d8b73ce46ec74de7ba37c03c3b
Pranav <pg1653@nyu.edu>
2021-04-20 19:20:04 -0400

    editing db.js file

--- back-end/db.js
+++ back-end/db.js
@@ -3,14 +3,6 @@ require('dotenv').config();
 
 const url = `mongodb+srv://${process.env.DB_EMAIL}:${process.env.DB_PASSWORD}@travelwise.dwvrv.mongodb.net/users?retryWrites=true&w=majority`;
 
-const User = new mongoose.Schema({
-    fullname: String,
-    email: String,
-    password: String
-});
-
-mongoose.model('User', User);
-
 mongoose.connect(url)
     .then(() => {
         console.log('Connected to database!')
