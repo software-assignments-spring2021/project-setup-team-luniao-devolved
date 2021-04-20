a7783f4368a78b2f18bc2e2e79ebbfb712a7316e
Pranav <pg1653@nyu.edu>
2021-04-20 10:40:58 -0400

    creating post schema

--- back-end/db.js
+++ back-end/db.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const url = `mongodb+srv://${process.env.DB_EMAIL}:${process.env.DB_PASSWORD}@travelwise.dwvrv.mongodb.net/users?retryWrites=true&w=majority`;
 
+
+/**** User ****/
 const User = new mongoose.Schema({
     fullname: String,
     email: String,
@@ -10,6 +12,20 @@ const User = new mongoose.Schema({
 });
 
 mongoose.model('User', User);
+
+
+/**** Post ****/
+const postSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    date: { type: Date, default: Date.now },
+    comments: [{body:"string", by: mongoose.Schema.Types.ObjectId}],
+    // postedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}, //comment out once passport is set up
+});
+
+mongoose.model('Post', postSchema);
+
+
 
 mongoose.connect(url)
     .then(() => {
