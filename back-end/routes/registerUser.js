bc74c21fcb07583c0fd4034a89c391eec440c8c2
Pranav <pg1653@nyu.edu>
2021-05-03 13:03:03 -0400

    Revert "adding validation to routes"
    This reverts commit 1105b375a2900be0c66f018595e7ba1f1e2fb056.

--- back-end/routes/registerUser.js
+++ back-end/routes/registerUser.js
@@ -2,47 +2,8 @@
 var User = require('../models/user');
 const passport = require('passport');
 
-
-
-// from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
-
-function validateFullName(name) {
-  const re = /^[a-zA-Z-'. ]+$/;
-  return re.test(String(name));
-}
-
-
 module.exports = app => {
   app.post('/api/signup', (req, res, next) => {
-
-    //if passwords are not the same
-    console.log(req.body.password);
-    console.log(req.body.repassword);
-
-    if (req.body.password !== req.body.repassword) {
-      res.send("incorrectpw");
-      return next(new Error([error]));
-    }
-
-
-    //email has to be a valid email
-    if (!validateEmail(req.body.email)) {
-      res.send("incorrectem");
-      return next(new Error([error]));
-    }
-
-
-    //if fullname is not a name (with each word starting with capital letters)
-    if (!validateFullName(req.body.fullname)) {
-      res.send("incorrectname");
-      return next(new Error([error]));
-    }
-
-
     passport.authenticate('register', (err, user, info) => {
       if (err) { console.log(err); }
       if (info != undefined) {
