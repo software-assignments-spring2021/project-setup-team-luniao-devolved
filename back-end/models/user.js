47339b5c394e553fff31a5ed250edbfc2d418f05
Pranav <pg1653@nyu.edu>
2021-05-03 16:06:10 -0400

    add friends to user schema

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -4,7 +4,8 @@ const User = new mongoose.Schema({
     fullname: String,
     email: String,
     password: String,
-    preference: [{type: mongoose.Schema.Types.ObjectId, ref: 'Pref'}]
+    preference: [{type: mongoose.Schema.Types.ObjectId, ref: 'Pref'}],
+    friends: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
 });
 
 module.exports = mongoose.model('User', User);
